5ca31821617da4f2625259fed55540914622d8e3
Kang younghun <dudgnskang99@ajou.ac.kr>
2025-10-02 15:37:57 +0900

    fear: add Hello script (v1)

---
+++ Assets/Scripts/Hello.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour
+{
+     void Start()
+    {
+        Debug.Log("Hello v1"); // 확인용 메시지
+    }
+}
